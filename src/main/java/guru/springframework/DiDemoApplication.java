7eda50369e04dac2494a673eaa72472bd2de56b9
pep <pep@mamut>
2018-04-02 22:01:58 +0200

    init

---
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -0,0 +1,17 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.controllers.MyController;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		MyController myController = (MyController)ctx.getBean("myController");
+		myController.hello();
+	}
+}
